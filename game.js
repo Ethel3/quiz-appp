92d6f818241551e10937ca270a1921b48cbb4d10
Ethel3 <akrasiethel4@gmail.com>
2022-04-23 20:51:12 +0000

    update

---
+++ game.js
@@ -0,0 +1,5 @@
+const question = document.querySelector("#question");
+const choices = Array.from[document.querySelectorAll(".choice-text")];
+const question = document.querySelector("#question");
+const question = document.querySelector("#question");
+const question = document.querySelector("#question");
