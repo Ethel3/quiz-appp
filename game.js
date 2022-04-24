97ad226403c5b44ca9254542f9fb9ac48a28bb73
Ethel3 <akrasiethel4@gmail.com>
2022-04-24 18:50:01 +0000

    updated

--- game.js
+++ game.js
@@ -3,3 +3,8 @@ const choices = Array.from[document.querySelectorAll(".choice-text")];
 const progressText = document.querySelector("#progressText");
 const scoreText = document.querySelector("#score");
 const progressBarFull = document.querySelector("#progressBarFull");
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
