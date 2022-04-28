109a9f96e2cd642e8fe26598b03e5aa8b4dea93e
Ethel3 <akrasiethel4@gmail.com>
2022-04-28 19:02:36 +0000

    updated

---
+++ js/questions.js
@@ -0,0 +1,36 @@
+let questions = [
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '31',
+        choice4: '5',
+        answer: 2,
+    },
+
+    {
+        question: 'Who is the current president of USA?',
+        choice1: 'Barak Obama',
+        choice2: 'Donald Trump',
+        choice3: 'Joe Biden ',
+        choice4: 'Queen Elizabeth',
+        answer: 3,
+    },
+
+    {
+        question: 'Which political party is ruling in Ghana currently?',
+        choice1: 'NPP',
+        choice2: 'PPP',
+        choice3: 'CPP',
+        choice4: 'NDC',
+        answer: 1,
+    },
+    {
+        question: 'what is the name of the Tallest Mountain in Ghana?',
+        choice1: 'Mount Afadja',
+        choice2: 'Mount Aduada',
+        choice3: 'Mount Atiwiredu',
+        choice4: 'Mount Kwamisa',
+        answer: 1,
+    },
+]
