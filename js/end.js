f6ac312ef92cb30f04cd89d64fa31400c2474813
Ethel3 <akrasiethel4@gmail.com>
2022-04-30 21:46:15 +0000

    updated

--- js/end.js
+++ js/end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username')
 const saveScore= document.querySelector('#saveScore')
 const finalScore= document.querySelector('#finalScore')
-const mostRecentScore = document.querySelector('#mostRecentScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
