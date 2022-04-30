98c4ab5124548d27d3901fa91639102858626980
Ethel3 <akrasiethel4@gmail.com>
2022-04-30 20:20:18 +0000

    updated

--- js/end.js
+++ js/end.js
@@ -9,7 +9,7 @@ const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
 
-username.addEventListener("keyup", [] => {
+username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value
 })
 
@@ -24,4 +24,13 @@ const score = {
 }
 
 highScores.push(score)
+
+highScores.sort((a,b) =>{
+    return b.score - a.score
+})
+
+highScores.splice(5)
+
+localStorage.setItem('highScores',  JSON.stringify(highScores))
+window.location.assign('/')
 }
