91173b1b7d8b20b9910977bb3aac278a34af60b9
Ethel3 <akrasiethel4@gmail.com>
2022-04-30 19:41:41 +0000

    updated

--- js/end.js
+++ js/end.js
@@ -2,3 +2,13 @@ const username = document.querySelector('#username')
 const saveScore= document.querySelector('#saveScore')
 const finalScore= document.querySelector('#finalScore')
 const mostRecentScore = document.querySelector('#mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+const MAX_HIGH_SCORES = 5
+
+finalScore.innerText = mostRecentScore
+
+username.addEventListener("keyup", [] => {
+    saveScoreBtn.disabled = !username
+})
