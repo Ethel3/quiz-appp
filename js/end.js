bddaa552e7af2208e56a40b3380eee07b874d62d
Ethel3 <akrasiethel4@gmail.com>
2022-04-30 19:20:20 +0000

    new

---
+++ js/end.js
@@ -0,0 +1,4 @@
+const username = document.querySelector('#username')
+const saveScore= document.querySelector('#saveScore')
+const finalScore= document.querySelector('#finalScore')
+const mostRecentScore = document.querySelector('#mostRecentScore')
