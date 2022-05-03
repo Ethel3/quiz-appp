36e2ac99af92e803d36b1564c88fae46c9000944
Ethel3 <akrasiethel4@gmail.com>
2022-05-03 17:16:59 +0000

    updated

--- js/highScores.js
+++ js/highScores.js
@@ -1,7 +1,7 @@
 const highScoresList = document.querySelector('#highscoresList')
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
-highScoresList.InnerHTML =
+highScoresList.innerHTML= 
 highScores.map(score => {
     return `<li class="high-score">${score.name}</li>`
 })
