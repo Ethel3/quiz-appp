4d5134dd59b66553fa4e17dd1336c4c8a282e534
Ethel3 <akrasiethel4@gmail.com>
2022-06-27 23:54:43 +0000

    updated

--- js/end.js
+++ js/end.js
@@ -5,7 +5,7 @@ const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
-const MAX_HIGH_SCORES = 5
+const MAX_HIGH_SCORES = 6
 
 finalScore.innerText = mostRecentScore
 
